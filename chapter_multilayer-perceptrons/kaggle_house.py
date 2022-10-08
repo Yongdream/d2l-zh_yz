b55730a804e82bb169b839b944d3b4127e7e2445
yongdream <youngzerooo@126.com>
2022-10-08 16:08:12 +0800

    test1609

--- chapter_multilayer-perceptrons/kaggle_house.py
+++ chapter_multilayer-perceptrons/kaggle_house.py
@@ -79,7 +79,7 @@ plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 
 # %%
-#异常值处理
+#异常值处理test
 figure=plt.figure()
 sns.pairplot(x_vars=['OverallQual','GrLivArea','YearBuilt','TotalBsmtSF'],
              y_vars=['SalePrice'],data=train_data,dropna=True)
