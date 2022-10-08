669a72e21f8b835f42200d90b1f244b60edc6fae
yongdream <youngzerooo@126.com>
2022-10-08 14:48:56 +0800

    1448

---
+++ chapter_linear-networks/import torch.py
@@ -0,0 +1,13 @@
+import torch
+from IPython import display
+from d2l import torch as d2l
+
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+num_input = 784
+num_outputs = 10
+
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
